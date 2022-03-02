5890e28d011ae41e32bf612b7f3a5e0660d627d0
Muhamad Firdaus <100768773+dauspairet@users.noreply.github.com>
2022-03-02 19:12:11 +0700

    Tugas individu nama

---
+++ Ini_Tugas.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "<h2>Saya Telah Mengerjakan Tugas Individual<?h2>";
+echo "<h2>2101995_Muhamad Firdaus_C2-Ilmu Komputer</h2>";
+?> 
+
+</body>
+</html>
